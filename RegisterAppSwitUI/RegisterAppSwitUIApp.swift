e584e96d9fe92825d65a0ee6aef63ea7799df24c
mohamed930 <mohammedali12477@gmail.com>
2025-06-24 10:40:45 +0300

    Initial Commit

---
+++ RegisterAppSwitUI/RegisterAppSwitUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  RegisterAppSwitUIApp.swift
+//  RegisterAppSwitUI
+//
+//  Created by Mohamed Ali on 24/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct RegisterAppSwitUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
